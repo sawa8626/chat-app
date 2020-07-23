0540e9f915803c5d75afd3b714b7e12a20bde804
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-07-23 19:11:04 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
   def index
     @room = Room.find(params[:room_id])
     @message = Message.new
+    @messages = @room.messages.includes(:user)
   end
 
   #メッセージの投稿
